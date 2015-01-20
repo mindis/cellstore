de4bf8bf735b0ac359ece20df7a4f1f63851bfcd
Dennis Knochenwefel <d@28.io>
2015-01-20 22:26:44 +0100

    :bug: refactor parameter handling :lipstick: some cleanup and sanity code.

--- app/api/api.js
+++ app/api/api.js
@@ -33,7 +33,6 @@ angular.module('report-editor')
                 param.description = $sce.trustAsHtml(param.description);
                 if(param.nameEditable){
                     param.placeholder = param.name;
-                    param.oldName = undefined;
                     param.name = undefined;
                 }
             })
@@ -46,7 +45,7 @@ angular.module('report-editor')
         $scope.API_URL = API_URL;
         $scope.api = swagger;
         $scope.path = path;
-        $scope.op = operation;
+        $scope.op = _.clone(operation);
         $scope.token = Session.getToken();
         $scope.params = {
             token: Session.getToken()
@@ -94,8 +93,44 @@ angular.module('report-editor')
             return 'curl -X ' + $scope.op.method + ' "' + $scope.getUrl() + '"';
         };
 
+        var getParamByPattern = function(name){
+            var paramsWithPatterns = _.filter($scope.op.parameters, function(param){
+                return _.isString(param.pattern);
+            });
+            var param = _.find(paramsWithPatterns, function(param){
+                var pattern = new RegExp(param.pattern);
+                return pattern.test( name );
+            });
+            if(_.isObject(param) && (_.isArray(param.value) || _.isString(param.value))){
+                // value is already in use
+                // need to add another parameter of the same kind
+                var newParam = _.clone(param);
+                newParam.name = name;
+                $scope.op.parameters.push(newParam);
+                return newParam;
+            } else if (_.isObject(param)){
+                param.name = name;
+            }
+            return param;
+        };
+
+        var getParam = function(name){
+            var param = _.find($scope.op.parameters, function(param){
+                return param.name === name;
+            });
+            if(_.isObject(param)){
+                return param;
+            }
+
+            // no direct match let's look for a pattern match
+            return getParamByPattern(name);
+        };
+
         $scope.test = function(p) {
             if(p) {
+                // reset
+                $scope.op = angular.copy(operation);
+
                 // make all values arrays
                 p = _.mapValues(p, function(paramValue){
                     if(_.isArray(paramValue)){
@@ -103,57 +138,37 @@ angular.module('report-editor')
                     }
                     return [paramValue];
                 });
-                $scope.op.parameters =
-                    _.chain($scope.op.parameters)
-                    .forEach(function(param){
-                        param.value = undefined;
-                        if(param.pattern){
-                            param.name = undefined;
-                        }
-                        var pattern = param.pattern ? new RegExp(param.pattern) : undefined;
-                        var patternMatches =
-                            pattern ?
-                                _.chain(_.pairs(p))
-                                .filter(function(pair){
-                                    return pattern.test( pair[0] );
-                                })
-                                .map(function(pair){
-                                    var key = pair[0];
-                                    var object = {};
-                                    object[key] = pair[1];
-                                    return object;
-                                })
-                                .value()[0]
-                            : undefined;
-                        if(p[param.name]){
-                            if(_.isArray(p[param.name])){
-                                param.value = p[param.name];
-                            } else {
-                                param.value = [p[param.name]];
-                            }
-                        } else if(patternMatches){
-                            param.value = _.chain(patternMatches).values().flatten().value();
-                            param.name = _.keys(patternMatches)[0];
-                        }
-                    })
-                    .value();
+                _.each(p, function(value, name){
+                    var param = getParam(name);
+                    if(_.isObject(param)){
+                        param.value = value;
+                    } else {
+                        $log.error(name + ": Parameter does not exist for operation.");
+                    }
+                });
             }
+            // $log.log(JSON.stringify($scope.op.parameters, null, '\t'));
             $scope.loading = true;
             $scope.error = undefined;
             $scope.body = undefined;
             var params = getParams();
-            API.Queries[operation.nickname](params)
-                .then(function(body){
-                    $scope.body = _.isString(body) ? body : JSON.stringify(body, null, 2);
-
-                })
-                .catch(function(error){
-                   $scope.body = _.isString(error.body) ? error.body : JSON.stringify(error.body, null, 2);
-                   $scope.error = error;
-                })
-                .finally(function(){
-                    $scope.loading = false;
-                });
+            if(_.keys(params).length > 1) {
+                API.Queries[operation.nickname](params)
+                    .then(function (body) {
+                        $scope.body = _.isString(body) ? body : JSON.stringify(body, null, 2);
+                    })
+                    .catch(function (error) {
+                        $scope.body = _.isString(error.body) ? error.body : JSON.stringify(error.body, null, 2);
+                        $scope.error = error;
+                    })
+                    .finally(function () {
+                        $scope.loading = false;
+                    });
+            } else {
+                $scope.error = "Too few parameters.";
+                $scope.body = "Too few parameters.";
+                $scope.loading = false;
+            }
         };
 
         if($scope.examples.length > 0) {
