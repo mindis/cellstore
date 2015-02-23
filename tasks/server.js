fe470a9888ae2f9c8cccb32d98b7bb54f47bc7aa
William Candillon <wcandillon@gmail.com>
2015-02-23 17:55:34 +0100

    :lipstick: Minor improvement

--- tasks/server.js
+++ tasks/server.js
@@ -51,6 +51,7 @@ gulp.task('server:dev', function() {
     gulp.watch(Config.paths.sass, ['sass', reload]);
     gulp.watch(Config.paths.js, reload);
     gulp.watch(Config.paths.json, ['jsonlint', reload]);
+    gulp.watch(Config.paths.documentation, ['gitbook', reload]);
     $28.watchJSONiqQueries();
 });
 
